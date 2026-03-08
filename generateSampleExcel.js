6ea1fb411bfdcd5697cc2558b39dad89b1e0d1be
veerababu525 <veerababu525@gmail.com>
2026-03-08 18:21:57 +0530

    fix: remove puppeteer and test files to fix Vercel install

--- generateSampleExcel.js
+++
@@ -1,8 +0,0 @@
-import * as XLSX from 'xlsx';
-import { products } from './src/data/productsData.js';
-
-const worksheet = XLSX.utils.json_to_sheet(products);
-const workbook = XLSX.utils.book_new();
-XLSX.utils.book_append_sheet(workbook, worksheet, "Products");
-XLSX.writeFile(workbook, "SampleProducts.xlsx");
-console.log("SampleProducts.xlsx created successfully! You can use this to test the Excel upload feature.");
